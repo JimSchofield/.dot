e07a2f1003261fbc52aa37db7c7216355b3c99c3
Jim Schofield <jschofie@nerdery.com>
2020-03-29 15:03:11 -0500

    Initial commit for .dot

---
+++ check_for_tools.sh
@@ -0,0 +1,25 @@
+# Checking for required tools.
+# In the future I think I'll do this in an install script, but for now it doesn't seem to
+# slow down terminal load 
+
+echo 'Checking for necessary tools...'
+
+command_exists() {
+    type "$1" > /dev/null 2>&1 || { echo >&2 "I require $1 but it doesn't exist! Make sure to intall it." }
+}
+
+required_apps=(
+    bat
+    fd
+    fzf
+    git
+    http-server
+    nvim
+    rg
+    tmux
+    vim
+)
+
+for app in "${required_apps[@]}"; do
+    type "$app" > /dev/null 2>&1 || { echo >&2 "$app doesn't exist! Make sure to intall it." } 
+done
